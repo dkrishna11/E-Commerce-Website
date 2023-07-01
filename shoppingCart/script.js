a59d34e6f504596559ecf6943d02b18a5be3f291
Krishna_D <dkrishna051997@gmail.com>
2023-07-01 23:54:49 +0530

    filter stage

--- shoppingCart/script.js
+++ shoppingCart/script.js
@@ -2,18 +2,30 @@ let mensCollection =document.getElementById("mens-collection");
 let womensCollection =document.getElementById("womens-collection");
 let electronics =document.getElementById("electronics");
 let jewelery =document.getElementById("jewelery");
+let results=[];
+let myCartArray = []; 
+
+
+let fetchData;
+let search=document.getElementsByClassName("fa-search")[0];
 
 const filterButtons=document.querySelectorAll("#select button");
 const filterableCart=document.querySelectorAll(".shopList .cart");
-console.log(filterableCart)
-let filterCart = e=>{
+
+let red=document.getElementById("red");
+let blue=document.getElementById("blue");
+let green=document.getElementById("green");
+let black=document.getElementById("black");
+let white=document.getElementById("white");
+
+// filter cart
+const filterCart = e=>{
     
     document.querySelector(".active").classList.remove("active");
     e.target.classList.add("active");
     
     filterableCart.forEach(card=>{
         card.classList.add("hide");
-        console.log(e.target.dataset.name)
         // console.log("card ",card.dataset.name)
         if(card.dataset.name===e.target.dataset.name || e.target.dataset.name==="all"){
             card.classList.remove("hide");
@@ -28,8 +40,10 @@ filterButtons.forEach(button => button.addEventListener("click", filterCart));
 async function fetchApi(){
     try{
         const response= await fetch("https://fakestoreapi.com/products");
-        const results= await response.json();
+         results= await response.json();
+        // fetchData=results;
         displayProduct(results)
+        
     }
     catch(Errors){
         console.log("Error:",Errors)
@@ -37,7 +51,7 @@ async function fetchApi(){
 }    
 
 function displayProduct(results){
-   
+    // fetchData=results;
     for(let i=0;i<results.length;i++){
         let category=results[i];
         if(category.category=="men's clothing"){
@@ -48,12 +62,16 @@ function displayProduct(results){
                         <p class="title">${category.title}</p>
                             <div class="price">
                                 <h4>$${category.price}</h4>
-                                <p>S, M, L</p>
+                                <p>${randomSize()}</p>
                             </div>
-                            <p>Color:</p>
+                           <div class="colorPick">
+                            <p style="background-color:${randomColor()};" class="color"></p>
+                            <p style="background-color:${randomColor()};" class="color"></p>
+                            <p style="background-color:${randomColor()};" class="color"></p>
+                           </div>
                             <p>Rating:${category.rating.rate}/5</p>
                         </div>
-                        <button class="addToCart">Add to Cart</button>
+                        <button class="addToCart" onclick="addToCarts(${category.id})">Add to Cart</button>
             `;
             div.className="productPoster";
             div.innerHTML=innerData;
@@ -67,12 +85,16 @@ function displayProduct(results){
                         <p class="title">${category.title}</p>
                             <div class="price">
                                 <h4>$${category.price}</h4>
-                                <p>S, M, L</p>
+                                <p>${randomSize()}</p>
                             </div>
-                            <p>Color:</p>
+                            <div class="colorPick">
+                            <p style="background-color:${randomColor()};" class="color"></p>
+                            <p style="background-color:${randomColor()};" class="color"></p>
+                            <p style="background-color:${randomColor()};" class="color"></p>
+                           </div>
                             <p>Rating:${category.rating.rate}/5</p>
                         </div>
-                        <button class="addToCart">Add to Cart</button>
+                        <button class="addToCart" onclick="addToCarts(${category.id})">Add to Cart</button>
             `;
             div.className="productPoster";
             div.innerHTML=innerData;
@@ -86,12 +108,16 @@ function displayProduct(results){
                         <p class="title">${category.title}</p>
                             <div class="price">
                                 <h4>$${category.price}</h4>
-                                <p>S, M, L</p>
+                                <p>${randomSize()}</p>
                             </div>
-                            <p>Color:</p>
+                            <div class="colorPick">
+                            <p style="background-color:${randomColor()};" class="color"></p>
+                            <p style="background-color:${randomColor()};" class="color"></p>
+                            <p style="background-color:${randomColor()};" class="color"></p>
+                           </div>
                             <p>Rating:${category.rating.rate}/5</p>
                         </div>
-                        <button class="addToCart">Add to Cart</button>
+                        <button class="addToCart" onclick="addToCarts(${category.id})">Add to Cart</button>
             `;
             div.className="productPoster";
             div.innerHTML=innerData;
@@ -105,12 +131,16 @@ function displayProduct(results){
                         <p class="title">${category.title}</p>
                             <div class="price">
                                 <h4>$${category.price}</h4>
-                                <p>S, M, L</p>
+                                <p>${randomSize()}</p>
                             </div>
-                            <p>Color:</p>
+                            <div class="colorPick">
+                            <p style="background-color:${randomColor()};" class="color"></p>
+                            <p style="background-color:${randomColor()};" class="color"></p>
+                            <p style="background-color:${randomColor()};" class="color"></p>
+                           </div>
                             <p>Rating:${category.rating.rate}/5</p>
                         </div>
-                        <button class="addToCart">Add to Cart</button>
+                        <button class="addToCart" onclick="addToCarts(${category.id})">Add to Cart</button>
             `;
             div.className="productPoster";
             div.innerHTML=innerData;
@@ -119,7 +149,51 @@ function displayProduct(results){
     }
 
 }
+// search the cart
+search.addEventListener("click", ()=>{
+    let searchData=document.getElementsByClassName("input-field")[0];
+   let value=searchData.value;
+   let title=document.querySelectorAll(".productPoster .title");
+   let elements=document.querySelectorAll(".productPoster");
 
+   title.forEach((ele, index)=>{
+    if(ele.innerText.includes(value)){
+        elements[index].classList.remove("hide");
+    }
+    else{
+        elements[index].classList.add("hide");
+    }
+   })
+});
+
+let temp=JSON.parse(localStorage.getItem("cart"))
+if(temp){
+  myCartArray=temp;
+}
+
+function addToCarts(itemId) {
+    let temp = results.filter((item) => {
+        return item.id == itemId;
+    });
+    
+    myCartArray.push(temp[0]);
+    localStorage.setItem("cart", JSON.stringify(myCartArray));
+}
+
+function randomColor(){
+    const colors = ["red", "blue", "black"];
+    const length = colors.length;
+    const randomIndex = Math.floor(Math.random() * length);
+    return colors[randomIndex];
+}
+
+function randomSize(){
+    const size = ["S", "M", "L"];
+    const length = size.length;
+    const randomIndex = Math.floor(Math.random() * length);
+    return size[randomIndex];
+}
+let color=randomColor();
 fetchApi();
 
 
